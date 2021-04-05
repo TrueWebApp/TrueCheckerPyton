754ddbc86be95d5fcc1f637248c7028e59142fd9
Oleg A <t0rr@mail.ru>
2021-04-05 19:35:15 +0300

    feat: add test params as fixtures from .env

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,41 @@ import pytest
 
 from truechecker import TrueChecker
 
-TOKEN = "1234567890:AnyValidToken"
+FILE_PATH = "tests/dataset/users.csv"
+
+
+@pytest.fixture(name="token")
+def token_fixture():
+    token = os.getenv("CHECKER_TOKEN")
+    if token is None:
+        RuntimeError("`CHECKER_TOKEN` environment variable is not found.")
+    return token
+
+
+@pytest.fixture(name="username")
+def username_fixture():
+    username = os.getenv("CHECKER_USERNAME")
+    if username is None:
+        RuntimeError("`CHECKER_USERNAME` environment variable is not found.")
+    return username
+
+
+@pytest.fixture(name="job_id")
+def job_id_fixture():
+    job_id = os.getenv("CHECKER_JOB_ID")
+    if job_id is None:
+        RuntimeError("`CHECKER_JOB_ID` environment variable is not found.")
+    return job_id
+
+
+@pytest.fixture(name="file_path")
+def file_path_fixture():
+    file_path = os.getenv("CHECKER_FILE_PATH", FILE_PATH)
+    return file_path
 
 
 @pytest.fixture(name="checker")
-async def checker_fixture():
-    token = os.getenv("TELEGRAM_TOKEN")
-    if token is None:
-        RuntimeError("`TELEGRAM_TOKEN` environment variable is not found.")
+async def checker_fixture(token):
     checker = TrueChecker(token)
     yield checker
     await checker.close()
