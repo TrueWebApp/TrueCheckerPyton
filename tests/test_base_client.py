27b798142327c12c87ed39183a33fae5466042dd
Oleg A <t0rr@mail.ru>
2021-04-06 14:09:39 +0300

    fix: init the session before double close check

--- tests/test_base_client.py
+++ tests/test_base_client.py
@@ -7,7 +7,10 @@ pytestmark = pytest.mark.asyncio
 
 class TestBaseClient:
     async def test_checker_double_close(self, checker: TrueChecker):
+        checker._get_session()
         await checker.close()
+        await checker.close()
+        assert checker._session.closed
 
     async def test_no_session_close(self, checker: TrueChecker):
         checker._session = None
