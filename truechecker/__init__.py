dda098a605ea43755b7bc7826c1c4d72dbaf76ff
Oleg A <t0rr@mail.ru>
2021-04-07 00:03:02 +0300

    refactor: remove redundant uvloop define

--- truechecker/__init__.py
+++ truechecker/__init__.py
@@ -6,9 +6,9 @@ from .api import TrueChecker
 
 # install uvloop if exists and not disabled
 try:
-    import uvloop
+    import uvloop  # noqa
 except ImportError:  # pragma: no cover
-    uvloop = None
+    pass
 else:
     if "DISABLE_UVLOOP" not in os.environ:
         uvloop.install()
