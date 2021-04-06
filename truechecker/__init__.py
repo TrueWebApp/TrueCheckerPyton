b5dffae7fe2e50a16fa82e7efeeca931260c8257
Oleg A <t0rr@mail.ru>
2021-04-06 19:53:58 +0300

    fix: enable uvloop

--- truechecker/__init__.py
+++ truechecker/__init__.py
@@ -1,3 +1,14 @@
 __all__ = ["TrueChecker"]
 
+import os
+
 from .api import TrueChecker
+
+# install uvloop if exists and not disabled
+try:
+    import uvloop
+except ImportError:  # pragma: no cover
+    uvloop = None
+else:
+    if "DISABLE_UVLOOP" not in os.environ:
+        uvloop.install()
